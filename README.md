8a295b2219cb6b7d43ef89ddcdb4d0b4431deb94
SeoHyeon <tjgus1668@naver.com>
2024-11-08 22:43:40 +0900

    WIP TP

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
   - [Profile](https://csh1668.github.io/Profile/index.html)
   - [Calender](https://csh1668.github.io/Calender/index.html)
   - [Media Animation (Lab Assignment 6)](https://csh1668.github.io/LA6/index.html)
-  - [Term Project (Unsolved)](https://csh1668.github.io/Unsolved-Build/index.html)
+  - [Term Project (Unsolved)](https://csh1668.github.io/Unsolved-Build/)
 - [Calculator](https://csh1668.github.io/Calculator/index.html)
--- unsolved/src/Components/MySidebar.js
+++ unsolved/src/Components/MySidebar.js
@@ -13,24 +13,22 @@ export default function MySidebar() {
                 <i class="fs-3 bi-check-circle-fill text-highlight-color"></i><span class="fs-5 d-none d-sm-inline ms-2">Unsolved</span>
                 </div>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-                    <li class="nav-item w-100 text-start">
-                        <Link to="/" class="nav-link px-0">
-                            <i class="fs-5 bi-house text-highlight-color"></i><span class="ms-1 d-none d-sm-inline text-basic-color">Home</span>
-                        </Link>
-                    </li>
-                    <li class="nav-item w-100 text-start">
-                        <Link to="/problems" class="nav-link px-0">
-                            <i class="fs-5 bi-list-check text-highlight-color"></i><span class="ms-1 d-none d-sm-inline text-basic-color">Problems</span>
-                        </Link>
-                    </li>
-                    <li class="nav-item w-100 text-start">
-                        <Link to='/community' class='nav-link px-0'>
-                            <i class="fs-5 bi-chat-dots text-highlight-color"></i><span class='ms-1 d-none d-sm-inline text-basic-color'>Community</span>
-                        </Link>
-                    </li>
+                    <LinkElement to='/' icon='house' text='Home' />
+                    <LinkElement to='/problems' icon='list-check' text='Problems' />
+                    <LinkElement to='/community' icon='chat-dots' text='Community' />
                 </ul>
             </div>
         </div>
         </>
+    );
+}
+
+function LinkElement(props) {
+    return (
+        <li class="nav-item w-100 text-start">
+            <Link to={props.to} class="nav-link px-0">
+                <i class={"fs-5 bi-" + props.icon + " text-highlight-color"}></i><span class="ms-1 d-none d-sm-inline text-basic-color">{props.text}</span>
+            </Link>
+        </li>
     );
 }
